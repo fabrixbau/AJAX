6cfae01f40ff79ed083671c039c3b8d801f557f9
Fabrizio <bautistafabrizio@gmail.com>
2023-10-21 10:41:04 -0600

    Agregue los métodos de como antes eran las peticiones, en la carpeta classic y también Agregue como se utilizaria FETCH

--- XMLHTTPREQUEST/script.js
+++ XMLHTTPREQUEST/script.js
@@ -14,3 +14,20 @@ btn.addEventListener("click", evt => {
 
     xhr.send()
 })
+
+
+/* para trabajar con FETCH (FRAMEWORK)
+ 
+Esta libreria es una alternativa de XMLHTTPREQUEST, y el código
+para sustituir la funcion que tengo arriba con XMLHTTPREQUEST, sería
+el siguiente.
+
+btn.addEventListener("click", evt => {
+    fetch("http://localhost:8080/tabla.html")
+    .then(response => response.text())
+    .then(response => {
+        dataConteiner.innerHTML = response
+    })
+    .catch(err => console.logt(err))
+
+*/
